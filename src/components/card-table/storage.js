bd5215f775b0cc76ab83cf7382478ba21b455b8a
Colin Bate <colin@colinbate.com>
2017-06-17 00:59:33 -0300

    Better state clear on reset. Still function without localStorage.

--- src/components/card-table/storage.js
+++ src/components/card-table/storage.js
@@ -1,3 +1,28 @@
 /* global  localStorage */
-exports.get = (key) => localStorage.getItem(key);
-exports.set = (key, value) => localStorage.setItem(key, value);
+let store;
+function testStorage () {
+  try {
+    const x = 'test-localstorage-' + Date.now();
+    localStorage.setItem(x, x);
+    const y = localStorage.getItem(x);
+    localStorage.removeItem(x);
+    if (y !== x) {
+      throw new Error();
+    }
+    store = localStorage; // localStorage is fully functional!
+  } catch (e) {
+    store = {
+      data: {},
+      getItem (key) {
+        return this.data[key];
+      },
+      setItem (key, val) {
+        this.data[key];
+      }
+    };
+  }
+}
+
+testStorage();
+exports.get = (key) => store.getItem(key);
+exports.set = (key, value) => store.setItem(key, value);
